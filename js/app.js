f1b7eef6ef5bb17d622db3975c13e14303341008
acai0 <alicecai08@hotmail.com>
2022-01-03 15:21:00 +0100

    nono fixe, touche du clavier fonctionnel

--- js/app.js
+++ js/app.js
@@ -6,9 +6,10 @@ const config = {
         default: 'arcade',
         arcade:{
             gravity: {
-                y: 300 //axe vertical
-                
-            }
+                y: 300 //axe vertical 
+            },
+                debug: false
+            
         }
     },
     scene: {
@@ -23,39 +24,49 @@ let cursors
 
 function preload() {
     console.log(this)
-    this.load.image('nono', 'assets/nono.png')
     this.load.image('vert', 'assets/pipe.png')
-    this.load.image('rouge', 'assetse/obstacle.png')
+    this.load.image('rouge', 'assets/obstacle.png')
+    this.load.spritesheet('nono', 'assets/nono.png', 
+    { frameWidth: 50, frameHeight: 48 }
+    );
 }
 function create() {
+    // position fixe de nono
+    nono = this.physics.add.image(500, 500, 'nono')
 
-    nono = this.physics.add.image(0, 0, 'nono')
-    nourriture = this.physics.add.image(50,50, 'vert')
-    obstacle = this.physics.add.image(50, 50, 'rouge')
+    nourriture = this.physics.add.image(200,200, 'vert')
+    nourriture = this.physics.add.image(50,70, 'vert')
+    nourriture = this.physics.add.image(400,400, 'vert')
+    obstacle = this.physics.add.image(500, 150, 'rouge')
+    obstacle = this.physics.add.image(100, 200, 'rouge')
 
-    nono.body.collideWorldBounds = true; //collision avec notre cadre
-    nourriture.body.collideWorldBounds = false;
-    //obstacle.body.collideWorldBounds = false;
-    //cursors = this.input.keyboard.createCursorKeys() //utilisation des touches clavier
-    //console.log(cursors)
-    //this.add.image(400, 300, 'bloc')
-    
+//collision avec le cadre
+nono.body.collideWorldBounds = true;
+nourriture.body.collideWorldBounds = false;
+obstacle.body.collideWorldBounds = false;
+
+
+cursors = this.input.keyboard.createCursorKeys() //utilisation des touches clavier
+console.log(cursors)
+//this.add.image(400, 300, 'bloc')
 }
 function update() {
-    /*
-    nono.setVelocityX(0)
 
-    if(cursors.up.isDown){ // si la touche du haut est appuyé
-    nono.setVelocity(0, -300) // vélosité 
+// Faire fonctionner avec les touches du clavier
+
+nono.setVelocityX(0)
+
+if(cursors.up.isDown){ // si la touche du haut est appuyé
+nono.setVelocity(0, -300) // vélosité 
 }
 if(cursors.right.isDown){
-        nono.setVelocity(300, 0)
+    nono.setVelocity(300, 0)
 }
 if(cursors.left.isDown){
-        nono.setVelocity(-300, 0)
+    nono.setVelocity(-300, 0)
 }
 if(cursors.down.isDown){
-        nono.setVelocity(0, 300)
+    nono.setVelocity(0, 300)
 }
-*/
+
 }
